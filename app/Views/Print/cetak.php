07657f4d5a270ab8bf3f8a16003b2a030930ffa3
Moch. Nurul Muaffan <muaffan92@gmail.com>
2021-04-05 15:27:13 +0700

    v1.1

--- app/Views/Print/cetak.php
+++ app/Views/Print/cetak.php
@@ -79,13 +79,19 @@
                 </div>
                 <div class="col-lg-3"></div>
                 <div class="col-lg-3"></div>
-                <div class="col-12 col-lg-3">
-                    <div class="row">
-                        <div class="col-12 col-lg-4 fw-bold header">Admin Bank</div>
-                        <div class="d-none d-lg-block col-lg-1">:</div>
-                        <div class="col text-dark">Rp. <?= number_format($admin, 2, '.', ',') ?></div>
+                <?php
+                if ($Transaksi['operator'] != 'ppob') {
+                ?>
+                    <div class="col-12 col-lg-3">
+                        <div class="row">
+                            <div class="col-12 col-lg-4 fw-bold header">Admin Bank</div>
+                            <div class="d-none d-lg-block col-lg-1">:</div>
+                            <div class="col text-dark">Rp. <?= number_format($admin, 2, '.', ',') ?></div>
+                        </div>
                     </div>
-                </div>
+                <?php
+                }
+                ?>
             </div>
             <div class="row">
                 <div class="col-12 col-lg-3">
@@ -246,7 +252,13 @@
                     </div>
                     <div class="col-lg-3"></div>
                     <div class="col-lg-3"></div>
-                    <div class="col-lg-3"></div>
+                    <div class="col-12 col-lg-3">
+                        <div class="row">
+                            <div class="col-12 col-lg-4 fw-bold header">Admin Bank</div>
+                            <div class="d-none d-lg-block col-lg-1">:</div>
+                            <div class="col text-dark">Rp. <?= number_format($admin, 2, '.', ',') ?></div>
+                        </div>
+                    </div>
                 </div>
             <?php
             }

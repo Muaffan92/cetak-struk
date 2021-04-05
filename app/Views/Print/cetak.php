b52745947595f1e2c8b98ce6a9ba9d3a86895ebe
Moch. Nurul Muaffan <muaffan92@gmail.com>
2021-04-05 14:39:53 +0700

    v1.1

--- app/Views/Print/cetak.php
+++ app/Views/Print/cetak.php
@@ -58,15 +58,13 @@
             $vsn = explode('/', $Transaksi['vsn']);
         ?>
             <div class="row">
-                <div class="col-2"></div>
                 <div class="col-4">
                     <h1 class="display-4 text-uppercase mt-5 mb-3">
                         <b>
-                            <strong><?= $Transaksi['operator'] ?></strong>
+                            CETAK STRUK PEMBAYARAN
                         </b>
                     </h1>
                 </div>
-                <div class="col-4"></div>
             </div>
             <div class="row">
                 <div class="col-12 col-lg-3">
@@ -239,7 +237,6 @@
                     <div class="col-4">
                         <p class="mx-auto header-center">TERIMAH KASIH</p>
                     </div>
-                    <div class="col-2"></div>
                 </div>
             </div>
         <?php

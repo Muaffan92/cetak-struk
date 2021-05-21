628660f5cdc5be95fe214fb139424d09c1c1f5ec
Moch. Nurul Muaffan <muaffan92@gmail.com>
2021-05-21 14:59:23 +0700

    pembaruan

--- app/Views/Print/ppob/speedy.php
+++ app/Views/Print/ppob/speedy.php
@@ -25,7 +25,7 @@
     <div class="container" id="section-to-print">
         <?php
         foreach ($getTransaksi as $Transaksi) {
-            if (($Transaksi['kode'] == 'tel')) {
+            if (($Transaksi['kode'] == 'speedy')) {
         ?>
                 <h1 class="mt-5 mb-3">
                     <b>Cetak Struk Pembayaran <?= $Transaksi['kode'] ?></b>
